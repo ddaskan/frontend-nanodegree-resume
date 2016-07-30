97c7368c0c4de04cee11b7e053ace1a8bc733b90
RiffinMerchant <riffinmerchant@gmail.com>
2016-07-30 15:47:35 -0400

    updated

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,26 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append("Dogan Askan");
- $("#header").append()
+ var firstName = "Dogan";
+ var lastName = "Askan";
+ var name = firstName+" "+lastName;
+ var age = 31;
+ var formattedName = HTMLheaderName.replace("%data%", name);
+ var formattedRole = HTMLheaderRole.replace("%data%", "Data Analyst");
+ var email = "doganaskan@gmail.com";
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+
+
+
+
+/*
+ $("#main").append(firstName+" "+lastName);
+ $("#header").append();
+console.log(lastName+firstName); */
+var awesomeThoughts = "I am "+firstName+" and I am AWESOME!";
+console.log(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace("AWESOME!", "FUN");
+ $("#main").append(funThoughts);
